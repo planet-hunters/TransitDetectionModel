af341035f761b2c1a5d5885595edf99fdef819e5
Advait Jigar Patel <apatel1@i27452.IMSA>
2022-04-06 02:28:02 -0500

    before downloading data

---
+++ preprocessing/get_data.sh
@@ -0,0 +1,13 @@
+# Filename containing the CSV file of TCEs in the training set.
+TCE_CSV_FILE="${HOME}/TCE_subset.csv"
+
+# Directory to download Kepler light curves into.
+KEPLER_DATA_DIR="${HOME}/kepler/"
+
+# Generate a bash script that downloads the Kepler light curves in the training set.
+python astronet/data/generate_download_script.py \
+  --kepler_csv_file=${TCE_CSV_FILE} \
+  --download_dir=${KEPLER_DATA_DIR}
+
+# Run the download script to download Kepler light curves.
+./get_kepler.sh
